8e3a3de325af6018db4bfb1eb3ba2e4136946523
Tunahan Akdoğan <tuna_han1999@hotmail.com>
2023-03-12 22:25:04 +0300

    Expense item modify

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+     const expenseDate = new Date(2021, 2, 28);
+     const expenseTitle = "Car Insurance";
+     const expenseAmount = 212.42;
      return (
           <div className="expense-item">
-               <div> March 28th 2021</div>
+               <div> {expenseDate.toString()}</div>
                <div className="expense-item__description">
-                    <h2>Car Insurance</h2>
+                    <h2>{expenseTitle}</h2>
                     <div className="expense-item__price">Amount</div>
                </div>
           </div>
